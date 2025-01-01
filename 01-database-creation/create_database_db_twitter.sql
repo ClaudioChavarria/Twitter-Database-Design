d01609e22a4e652ce3d5b8951a0dfe0c37b27b4d
ClaudioChavarria <109621712+ClaudioChavarria@users.noreply.github.com>
2025-01-01 14:10:25 -0600

    feat(database): create SQL script to initialize PostgreSQL database

---
+++ 01-database-creation/create_database_db_twitter.sql
@@ -0,0 +1,15 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS "db_twitter";
+
+-- Create the db_twitter database using template0 and UTF8 encoding
+CREATE DATABASE "db_twitter"
+    WITH
+        OWNER = postgres
+        ENCODING = 'UTF8'
+        LC_COLLATE = 'en_US.UTF-8'  -- Especifica una colación en UTF-8
+        LC_CTYPE = 'en_US.UTF-8'    -- Establece la ctype en UTF-8
+        LOCALE_PROVIDER = 'libc'
+        TABLESPACE = pg_default
+        CONNECTION LIMIT = -1
+        IS_TEMPLATE = False
+        TEMPLATE = template0;
