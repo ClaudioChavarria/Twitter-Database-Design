2fa58576e06776f437b0c910b91ba5704189f49a
ClaudioChavarria <109621712+ClaudioChavarria@users.noreply.github.com>
2025-01-01 14:42:03 -0600

    chore(database): remove Spanish comments from 'INSERT' script for 'notifications' table

--- 05-insert-data/10-Insert-social.notifications.sql
+++ 05-insert-data/10-Insert-social.notifications.sql
@@ -1,17 +1,17 @@
 INSERT INTO social.notifications (user_id, notification_type, reference_id)
 SELECT DISTINCT
-    t.user_id AS user_id, -- El autor del tweet
+    t.user_id AS user_id, 
     'like' AS notification_type,
     l.tweet_id AS reference_id
 FROM social.tweets t
 JOIN social.tweet_likes l ON t.tweet_id = l.tweet_id;
 
--- Notificaciones basadas en comentarios
 INSERT INTO social.notifications (user_id, notification_type, reference_id)
 SELECT DISTINCT
-    t.user_id AS user_id, -- El autor del tweet
+    t.user_id AS user_id, 
     'comment' AS notification_type,
     c.tweet_id AS reference_id
 FROM social.tweets t
-JOIN social.comments c ON t.tweet_id = c.tweet_id;
+JOIN social.comments c ON t.tweet_id = c.tweet_id
+;
 
